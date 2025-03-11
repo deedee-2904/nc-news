0a79d3049b4ac5de12aff655d88bc1e13c343b61
deedee-2904 <demaradarkwah@mail.com>
2025-03-11 09:39:54 +0000

    ticket 4: resoponds with array of article objects sorted in desc order by created_at property

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,8 @@ const app = require("../app.js");
 const seed = require("../db/seeds/seed.js");
 const data = require("../db/data/test-data");
 const db = require("../db/connection.js");
+require("jest-sorted");
+require("jest-extended");
 
 beforeEach(() => {
   return seed(data);
@@ -93,23 +95,35 @@ describe("GET /api/articles/:article_id", () => {
 });
 
 describe("GET /api/articles", () => {
-  test.todo("200 : Responds with an array of article objects sorted by date in descending order by default");
-  test('200 : Responds with an array of article objects sorted by date in descending order by default', () => {
+  test("200 : Responds with an array of article objects sorted by date in descending order by default", () => {
     return request(app)
       .get("/api/articles")
       .expect(200)
       .then(({ body }) => {
         const articles = body.articles;
+        console.log(articles)
         expect(articles.length).not.toBe(0);
-        // articles.forEach((article) => {});
+        articles.forEach((article) => {
+          const {
+            article_id,
+            title,
+            topic,
+            author,
+            created_at,
+            votes,
+            article_img_url,
+            comment_count,
+          } = article;
+          expect(typeof article_id).toBe("number");
+          expect(typeof title).toBe("string");
+          expect(typeof topic).toBe("string");
+          expect(typeof author).toBe("string");
+          expect(typeof created_at).toBe("string");
+          expect(typeof votes).toBe("number");
+          expect(typeof article_img_url).toBe("string");
+          expect(typeof comment_count).toBe("number");
+        });
+        expect(articles).toBeSortedBy("created_at", { descending: true });
       });
   });
-  test.todo("200 : Responds with an array of article objects sorted by the input table coloumn if it exists in the database");
-  test.todo("200 : Repsonds with an array of article objects sorted by date in ascending order queried by the client")
-  test.todo("400 : Responds with error message if topic doesn't exist in the database");
-  test.todo(
-    "404 : Responds with not found error if topic exists in the database but has no associated topics"
-  );
-  test.todo("400 : Responds with error message if sort by coloumn doesn't exist in the database");
-  test.todo("400 : Responds with error message if input sort order isn't ASC or DESC");
 });
--- models/fetchAllArticles.js
+++ models/fetchAllArticles.js
@@ -1,16 +1,16 @@
-const db = require("../db/connection")
+const db = require("../db/connection");
 
-exports.fetchAllArticles = ()=>{
-    // We need may have to join the articles table with the comments id where there's a matching article_id
-    // We need to select all coloumns excluding the body when we return the array of objects 
-
-    //SELECT * FROM articles, COUNT(article_id) AS comment_count
-    //FROM articles LEFT JOIN comments ON comments.article_id = articles.article_id
-    //GROUP BY article_id
-    return db.query(`SELECT COUNT(articles.article_id) AS comment_count FROM articles 
+exports.fetchAllArticles = () => {
+  return db
+    .query(
+      `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url,
+    COUNT(articles.article_id)::INT AS comment_count FROM articles 
     LEFT JOIN comments ON comments.article_id = articles.article_id
-    GROUP BY articles.article_id`).then(({rows})=>{ // only returns comment counts and doesn't like select all notataion
-        console.log(rows) // only returns comment
-        return rows
-    })
-}
+    GROUP BY articles.article_id
+    ORDER BY articles.created_at DESC`
+    )
+    .then(({ rows }) => {
+      const articles = rows;
+      return articles;
+    });
+};
