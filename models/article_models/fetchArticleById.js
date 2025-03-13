96eb5f3225fd29d4e54158810381b6cf4ea79d7d
deedee-2904 <demaradarkwah@mail.com>
2025-03-13 16:52:30 +0000

    ticket 12: get article by article_id with new comment_count property

--- models/article_models/fetchArticleById.js
+++ models/article_models/fetchArticleById.js
@@ -1,12 +1,22 @@
-const db = require("../../db/connection")
+const db = require("../../db/connection");
 
 exports.fetchArticleById = (article_id) => {
-  return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
-  .then(({ rows }) => {
-    const article = rows[0];
-    if (!article) {
-      return Promise.reject({ status: 404, msg: `No article found for article_id: ${article_id}` });
-    }
-    return article;
-  });
+  return db
+    .query(
+      `SELECT articles.*, COUNT(comments.article_id)::INT AS comment_count FROM articles 
+    LEFT JOIN comments ON comments.article_id = articles.article_id 
+    WHERE articles.article_id = $1
+    GROUP BY articles.article_id`,
+      [article_id]
+    )
+    .then(({ rows }) => {
+      const article = rows[0];
+      if (!article) {
+        return Promise.reject({
+          status: 404,
+          msg: `No article found for article_id: ${article_id}`,
+        });
+      }
+      return article;
+    });
 };
