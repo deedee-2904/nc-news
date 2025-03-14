8945fb5dd776d2e295fd933873ac99a72c991197
deedee-2904 <demaradarkwah@mail.com>
2025-03-14 17:06:29 +0000

    ticket 16: get user object by username

--- routes/users-router.js
+++ routes/users-router.js
@@ -1,8 +1,13 @@
 const usersRouter = require("express").Router()
 const { getAllUsers } = require("../controllers/user_controllers/getAllUsers")
+const { getUserByUsername } = require("../controllers/user_controllers/getUserByUsername")
 
 usersRouter
 .route("/")
 .get(getAllUsers)
+
+usersRouter
+.route("/:username")
+.get(getUserByUsername)
 
 module.exports = usersRouter
